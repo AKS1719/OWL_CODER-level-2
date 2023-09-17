12b7be0b63e8f6b6cf084e7588da4a953c6b0048
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2023-09-17 16:03:57 +0530

    Create code.cpp

---
+++ multiplication_table_within_range/code.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long int a,b,c;
+    cin >> a >> b >> c;
+    for(int i=b;i<=c;i++)
+    {
+        cout<<a<<" x "<<i<<" = "<<a*i<<endl;
+    }
+}
