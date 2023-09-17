eb0a98f61785952c990eb519bd72cdd3b8e064f4
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2023-09-17 16:43:40 +0530

    Create code.cpp

---
+++ Expression_Evaluation/code.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >>t;
+    while(t--)
+    {
+        long long int a,b,c;
+        cin >>a >> b >> c;
+        
+        if(a+b+c==0 or a+b-c==0 or a-b+c==0 or a-b-c==0)cout<<"YES";
+        else cout<<"NO";
+        cout<<endl;
+    }
+}
