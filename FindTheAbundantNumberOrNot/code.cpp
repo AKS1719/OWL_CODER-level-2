1d8285fbf827d385e3ed3ddc28987d0023252bcc
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2023-09-23 20:44:50 +0530

    Create code.cpp

---
+++ FindTheAbundantNumberOrNot/code.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int s=0;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            s+=i;
+        }
+    }
+    if(s>n)cout<<"True";
+    else cout<<"False";
+}
