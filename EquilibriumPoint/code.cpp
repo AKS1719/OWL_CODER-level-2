611569a71362401461cf6c55e5e63726f22e1052
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2023-09-28 12:37:04 +0530

    Create code.cpp

---
+++ EquilibriumPoint/code.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for(int i = 0 ; i < n;i++)
+        {
+            cin >> v[i];
+        }
+        int lsum =0,rsum =0;
+        int l = 0 , r = n-1;
+        int res = -1;
+        while(l<=r)
+        {
+            if(l==r)
+            {
+                if(lsum==rsum)res=l;
+                break;
+            }
+            else if(l==0 and r==n-1)
+            {
+                lsum+=v[l++];
+                rsum+=v[r--];
+            }
+            else if(lsum<=rsum)
+            {
+                lsum+=v[l++];
+            }
+            else if(rsum<=lsum)
+            {
+                rsum+=v[r--];
+            }
+        }
+        cout<<res+1<<endl;
+    }
