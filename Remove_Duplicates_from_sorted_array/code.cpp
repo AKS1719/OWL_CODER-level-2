cad9b673c25fe23232437a064eec10d2ed70e4db
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2023-09-30 16:04:18 +0530

    Create code.cpp

---
+++ Remove_Duplicates_from_sorted_array/code.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    set<int> s;
+    for(int i = 0 ; i < n ;i++)
+    {
+        int a;
+        cin >> a;
+        s.insert(a);
+    }
+    for(auto &x : s)cout<<x<<" ";
+}
